c7458bbf0ee25bbb407ed3718fdb68699f11a4cc
OH-JINHYEON <jinibrighteveryday@gmail.com>
2025-05-18 16:30:11 +0900

    Reminding and practicing Algorithm 2025.05.18

--- src/JAVA_ALGORITHM/JudgeSign.java
+++ src/JAVA_ALGORITHM/JudgeSign.java
@@ -6,20 +6,15 @@ public class JudgeSign {
 
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
+        System.out.println("정수를 입력하세요.: ");
 
-        System.out.println("정수를 입력하시오.");
         int n = stdIn.nextInt();
 
         if (n > 0)
             System.out.println("이 수는 양수입니다.");
-
         else if (n < 0)
             System.out.println("이 수는 음수입니다.");
-
-        // if (n < 0) 비교군 대조해야 함
-            //System.out.println("이 수는 음수입니다.");
-
         else
-            System.out.println("이 수는 0 입니다.");
+            System.out.println("이 수는 0입니다.");
     }
 }
--- src/JAVA_ALGORITHM/PrintStars2.java
+++ src/JAVA_ALGORITHM/PrintStars2.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class PrintStars2 {
     public static void main(String[] args) {
-        Scanner stdIn = new Scanner(System.in);
-        int n, w;
-
-        System.out.println("*를 n개 출력하되, w개마다 줄을 바꿔서 출력합니다.");
-
-        for (int i = 0; i < n / w; i++)
-            // java: variable n might not have been initialized
-            // https://wakestand.tistory.com/430 에러 참조
-            System.out.println("*".repeat(w));
-
-        int rest = n % w;
-        if (rest != 0)
-            System.out.println("*".repeat(rest));
+//        Scanner stdIn = new Scanner(System.in);
+//        int n, w;
+//
+//        System.out.println("*를 n개 출력하되, w개마다 줄을 바꿔서 출력합니다.");
+//
+//        for (int i = 0; i < n / w; i++)
+//            // java: variable n might not have been initialized
+//            // https://wakestand.tistory.com/430 에러 참조
+//            System.out.println("*".repeat(w));
+//
+//        int rest = n % w;
+//        if (rest != 0)
+//            System.out.println("*".repeat(rest));
     }
 }
